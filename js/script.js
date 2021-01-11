0267f299033721c4bc7757f607a7ee8110ffcc80
TheoLeao <theoleaoboudier@gmail.com>
2021-01-11 14:58:30 +0100

    MAJ

--- js/script.js
+++ js/script.js
@@ -26,7 +26,6 @@ document.querySelector('#inputAdresse').addEventListener('change', function (){
     lon = parseFloat(tabCoordonnees[0]);
     lat = parseFloat(tabCoordonnees[1]);
     //Affichage de la carte
-    //Affichage de la carte
     let mymap = L.map('map').setView([lon, lat], 13);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
