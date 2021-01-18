f1dabc6168a0d63b65710c0596f8e1ab465cf3f0
TheoLeao <theoleaoboudier@gmail.com>
2021-01-18 14:05:31 +0100

    amélioration des fonctions avec des Promess JS

--- js/script.js
+++ js/script.js
@@ -1,13 +1,6 @@
-/*let mymap = L.map('map').setView([46.227638,2.213749], 13);
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 6,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoidGhlb2xlYW8iLCJhIjoiY2tpZG91MDJzMWw2MDJ4bzVianp6cXBsaCJ9.ps_BFy88xj0l6kMkf9ivgA'
-}).addTo(mymap);
-*/
+function CL(message) {
+
+}
 
 
 //https://nominatim.openstreetmap.org/search?q=17+Strada+Pictor+Alexandru+Romano%2C+Bukarest&format=geojson
@@ -21,22 +14,21 @@ hiddenInput = document.getElementById(document.querySelector('#inputAdresse').ge
 
 
     //Ecouteurs d'evenements
-    document.querySelector('#inputAdresse').addEventListener('input', function () { autocompletionInput(inputAdresse, datalistInputAdresse, zoneAlert, 'https://nominatim.openstreetmap.org/search', { 'street': inputAdresse.value, 'country': 'France', format: 'json' }) })
+    window.addEventListener("load", function (event) {
+
+    });
+document.querySelector('#inputAdresse').addEventListener('input', function () { autocompletionInput(inputAdresse, datalistInputAdresse, zoneAlert, 'https://nominatim.openstreetmap.org/search', { 'street': inputAdresse.value, 'country': 'France', format: 'json' }) })
 document.querySelector('#inputAdresse').addEventListener('change', function () {
+
     tabCoordonnees = hiddenInput.value.split(',');
     lon = parseFloat(tabCoordonnees[0]);
     lat = parseFloat(tabCoordonnees[1]);
-    //Affichage de la carte
+    showMap(lon, lat).then(() => {
+        //console.log(showStations(48.5833, 7.75, 44.8333, -0.5667));
+        //alert('show map ok');
+        console.log('ok');
+    }, () => { console.log('probleme affichage') });
 
-    let mymap = L.map('map', {zoomControl: true}).setView([lon, lat], 13);
-    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        maxZoom: 12,
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: 'pk.eyJ1IjoidGhlb2xlYW8iLCJhIjoiY2tpZG91MDJzMWw2MDJ4bzVianp6cXBsaCJ9.ps_BFy88xj0l6kMkf9ivgA'
-    }).addTo(mymap);
 })
 //Stocker la valeur de l'option selectionné dans la hiddenInput pour la recuperer au input
 document.querySelector('input[list]').addEventListener('input', function (e) {
@@ -47,6 +39,7 @@ document.querySelector('input[list]').addEventListener('input', function (e) {
         inputValue = input.value;
 
     hiddenInput.value = inputValue;
+    //console.log('hiddenInput: ' + hiddenInput.value)
 
     for (var i = 0; i < options.length; i++) {
         var option = options[i];
@@ -106,10 +99,9 @@ function autocompletionInput(nodeInput, nodeDatalist, nodeAlert, urlReq, paramsR
                     Les villes s'afficheront uniquement à partir de 3 caractères.
                 </div>`;
     }
-    console.log('ok')
 }
 
-
+/*Récupération du accessToken
 function getAccessToken_NetatmoApi() {
     var myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
@@ -129,51 +121,80 @@ function getAccessToken_NetatmoApi() {
 
     fetch("https://api.netatmo.com/oauth2/token", requestOptions)
         .then(response => response.text())
-        .then(result => console.log(JSON.parse(result).access_token))
-        .catch(error => console.log('error', error));
-}
-function getStations(lat_NE, lon_NE, lat_SW, lon_SW) {
-    var myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer 5ffc5befccb0da524b20861c|f9055d4e55d4901c71893c23b1a4e1e7");
-    
-    var urlencoded = new URLSearchParams();
-    
-    var requestOptions = {
-      method: 'POST',
-      headers: myHeaders,
-      body: urlencoded,
-      redirect: 'follow'
-    };
-    
-    fetch(`https://api.netatmo.com/api/getpublicdata?lat_ne=${lat_NE}&lon_ne=${lon_NE}&lat_sw=${lat_SW}&lon_sw=${lon_SW}&filter=false`, requestOptions)
-      .then(response => response.text())
-      .then(result => console.log(result))
-      .catch(error => console.log('error', error));
-}
+        //.then(result => console.log(JSON.parse(result).access_token))
+        .then(function (result) {
+            const token = JSON.parse(result).access_token;
 
-function showStations(urlReq, paramsReq) {
-    $get(urlReq, paramsReq)
-        .then((req) => {
-            //Convertir la réponse text en JSON
-            data = JSON.parse(req.responseText);
-            console.log(data);
-            //Afficher les bornes
-            for (i = 0; i < data.length; i++) {
-
-            }
         })
-        .catch((req) => {
-            //Affichage de l'erreur récupération BDD
-            document.getElementsByClassName('zoneAlert')[0].innerHTML = '';
-            //Afficher le message d'erreur
-            document.getElementsByClassName('zoneAlert')[0].innerHTML += `
-        <div class="alert alert-danger" role="alert">
-            L'affichage des bornes météos netatmo est indisponible.
-        </div>`;
-        });
+        .catch(error => console.log('error', error));
+}*/
+
+function showStations(lat_NE, lon_NE, lat_SW, lon_SW) {
+    //Récupération du refresh_token
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+
+    var urlencoded = new URLSearchParams();
+    urlencoded.append("grant_type", "refresh_token");
+    urlencoded.append("refresh_token", "5ffc5befccb0da524b20861c|20832aa7e0dfa5873598919b947e62a1");
+    urlencoded.append("client_id", "5ffc5c2cbd10c6698151b1cf");
+    urlencoded.append("client_secret", "NfDkkPDA6qEiQ25HwOHqsIvsOCybFhL5GgBOTF8");
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: urlencoded,
+        redirect: 'follow'
+    };
+
+    fetch("https://api.netatmo.com/oauth2/token", requestOptions)
+        .then(response => response.text())
+        //.then(result => console.log(JSON.parse(result).access_token))
+        .then(function (result) {
+            const token = JSON.parse(result).access_token;
+
+            var myHeaders = new Headers();
+
+            //Récupération des bornes méteos
+            myHeaders.append("Authorization", `Bearer ${token}`);
+            var urlencoded = new URLSearchParams();
+
+            var requestOptions = {
+                method: 'POST',
+                headers: myHeaders,
+                body: urlencoded,
+                redirect: 'follow'
+            };
+
+            //Récupération des données NetATMO
+            fetch(`https://api.netatmo.com/api/getpublicdata?lat_ne=${lat_NE}&lon_ne=${lon_NE}&lat_sw=${lat_SW}&lon_sw=${lon_SW}&filter=false`, requestOptions)
+                .then(response => response.text())
+                .then(result => console.log(JSON.parse(result)))
+                .catch(error => console.log('error', error));
+
+        })
+        .catch(error => console.log('error netatmo', error));
+
+
 }
-getStations(48.5833, 7.75, 44.8333, -0.5667);
-accessToken = getAccessToken_NetatmoApi();
-//console.log(accessToken)
-showStations();
-getStations();
+
+
+function showMap(lon, lat) {
+    return new Promise((successCallback, failureCallback) => {
+        //Affichage de la carte
+        let mymap = L.map('map', { zoomControl: true }).setView([lon, lat], 13);
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 12,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'pk.eyJ1IjoidGhlb2xlYW8iLCJhIjoiY2tpZG91MDJzMWw2MDJ4bzVianp6cXBsaCJ9.ps_BFy88xj0l6kMkf9ivgA'
+        }).addTo(mymap);
+    })
+}
+
+console.log(showStations(48.5833, 7.75, 44.8333, -0.5667));
+
+
+alert('test');
